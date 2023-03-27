dcc9de2507c15ed5923c64f113d52fa216d229fe
Sahil S <sahil07052002@gmail.com>
2023-03-27 09:26:55 +0530

    Finished Book Routes

--- routes/authors.js
+++ routes/authors.js
@@ -2,110 +2,99 @@ const express = require('express')
 const router = express.Router()
 const Author = require('../models/author')
 const Book = require('../models/book')
-// All authors routes
-router.get('/' , async (req , res) => {
-    let searchOptions = {}
-    if(req.query.name !== null && req.query.name !== ''){
-        searchOptions.name = new RegExp(req.query.name,'i')
-    }
-    try{
-        const authors = await Author.find(searchOptions)
-        res.render('authors/index', {
-            authors: authors
-            , searchOptions: req.query
-        })
-    }
-    catch {
-        res.redirect('/')
-    }
+
+// All Authors Route
+router.get('/', async (req, res) => {
+  let searchOptions = {}
+  if (req.query.name != null && req.query.name !== '') {
+    searchOptions.name = new RegExp(req.query.name, 'i')
+  }
+  try {
+    const authors = await Author.find(searchOptions)
+    res.render('authors/index', {
+      authors: authors,
+      searchOptions: req.query
+    })
+  } catch {
+    res.redirect('/')
+  }
 })
 
 // New Author Route
-router.get('/new' , (req,res) => {
-    res.render('authors/new' , {author: new Author()});
+router.get('/new', (req, res) => {
+  res.render('authors/new', { author: new Author() })
 })
 
-// Creating Author Route
-// async is usedto wait for the response from the database
-router.post('/' , async (req,res) => {
-    const author = new Author({
-        name: req.body.name
+// Create Author Route
+router.post('/', async (req, res) => {
+  const author = new Author({
+    name: req.body.name
+  })
+  try {
+    const newAuthor = await author.save()
+    res.redirect(`authors/${newAuthor.id}`)
+  } catch {
+    res.render('authors/new', {
+      author: author,
+      errorMessage: 'Error creating Author'
     })
-    try {
-        const newAuthor = await author.save()
-        // await is used to wait for the response from the database
-
-        res.redirect(`authors/${newAuthor.id}`)
-    } catch {
-        res.render('authors/new', {
-            author:author,
-            errorMessage: "Error creating author"
-        })
-    }
-    
+  }
 })
 
-router.get('/:id' , async (req,res) => {
-    try {
-        const author = await Author.findById(req.params.id)
-        const books = await Book.find({ author: author.id}).limit(6).exec()
-        res.render('authors/show', {
-            author: author,
-            booksByAuthor: books
-        })
-    }
-    catch {
-        res.redirect('/')
-    }
+router.get('/:id', async (req, res) => {
+  try {
+    const author = await Author.findById(req.params.id)
+    const books = await Book.find({ author: author.id }).limit(6).exec()
+    res.render('authors/show', {
+      author: author,
+      booksByAuthor: books
+    })
+  } catch {
+    res.redirect('/')
+  }
 })
 
-router.get('/:id/edit' , async ( req,res) => {
-    try {
-        const author = await Author.findById(req.params.id)
-        res.render('authors/edit' , {author: author})
-    }
-    catch {
-        res.redirect('/authors')
-    }
+router.get('/:id/edit', async (req, res) => {
+  try {
+    const author = await Author.findById(req.params.id)
+    res.render('authors/edit', { author: author })
+  } catch {
+    res.redirect('/authors')
+  }
 })
 
-router.put('/:id' , async (req,res) => {
-    let author
-    try {
-        // find the author by id
-        author = await author.findById(req.params.id)
-        author.name = req.body.name
-        await author.save() // save the updated author
-        res.redirect(`/authors/${author.id}`)
-    } catch {
-        if(author == null){
-            res.redirect('/')
-        }
-        else{
-        res.render('authors/edit', {
-            author:author,
-            errorMessage: "Error updating author"
-        })
-        }
+router.put('/:id', async (req, res) => {
+  let author
+  try {
+    author = await Author.findById(req.params.id)
+    author.name = req.body.name
+    await author.save()
+    res.redirect(`/authors/${author.id}`)
+  } catch {
+    if (author == null) {
+      res.redirect('/')
+    } else {
+      res.render('authors/edit', {
+        author: author,
+        errorMessage: 'Error updating Author'
+      })
     }
-    
+  }
 })
-// delete author and books 
-router.delete('/:id' , async (req,res) => {
-    let author
-    try {
-        // find the author by id
-        author = await author.findById(req.params.id)
-        await author.remove() 
-        res.redirect(`/authors`)
-    } catch {
-        if(author == null){
-            res.redirect('/')
-        }
-        else{
-        res.redirect(`authors/${author.id}`)
-        }
+
+router.delete('/:id', async (req, res) => {
+  let author
+  try {
+    author = await Author.findById(req.params.id)
+    await author.remove()
+    res.redirect('/authors')
+  } catch {
+    if (author == null) {
+      res.redirect('/')
+    } else {
+      res.redirect(`/authors/${author.id}`)
     }
+  }
 })
 
 module.exports = router
